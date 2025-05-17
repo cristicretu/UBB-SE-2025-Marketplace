83ba37a2979ecee903402fac28574e803423f971
Cristian Crețu <crisemcr@gmail.com>
2025-05-17 17:30:44 +0300

    add patapim (first) migration (#10)

--- server/Migrations/20250517132107_..Designer.cs
+++ server/Migrations/20250517132107_..Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Server.DataAccessLayer;
 
@@ -11,9 +12,11 @@ using Server.DataAccessLayer;
 namespace server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250517132107_.")]
+    partial class _
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
