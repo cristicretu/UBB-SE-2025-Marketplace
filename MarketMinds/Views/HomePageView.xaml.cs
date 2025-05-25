70906a069dcc5b150070619d06ac645674ae96af
alexapvl <alex.robite@gmail.com>
2025-05-25 13:32:09 +0300

    made the primary windows which open to have a min height and width (1/3 of the screen) and also open as 2/3 of the screen, for the login I enabled scrolling of the login and register forms so that even if the window is small, the user can still login

--- MarketMinds/Views/HomePageView.xaml.cs
+++ MarketMinds/Views/HomePageView.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Windowing;
 
 namespace MarketMinds.Views
 {
@@ -24,16 +25,88 @@ namespace MarketMinds.Views
         private const int SELLER_TYPE = 3;
         private bool isBuyer;
         private bool isSeller;
+        private DispatcherTimer _resizeTimer;
 
         public HomePageView()
         {
             this.InitializeComponent();
+
+            // Initialize resize timer
+            _resizeTimer = new DispatcherTimer();
+            _resizeTimer.Interval = TimeSpan.FromMilliseconds(300); // Wait 300ms after resize stops
+            _resizeTimer.Tick += ResizeTimer_Tick;
+
+            // Set initial window size to 2/3 of screen and handle minimum size constraints
+            SetInitialWindowSize();
+            this.SizeChanged += HomePageView_SizeChanged;
 
             // Navigate to the MarketMindsPage by default
             ContentFrame.Navigate(typeof(MarketMindsPage));
 
             // Configure UI based on user type
             ConfigureUIForUserType();
+        }
+
+        private void SetInitialWindowSize()
+        {
+            // Get the display area of the primary monitor
+            var displayArea = DisplayArea.Primary;
+            var workArea = displayArea.WorkArea;
+
+            // Calculate 2/3 of screen size for initial window
+            int initialWidth = (int)(workArea.Width * 2.0 / 3.0);
+            int initialHeight = (int)(workArea.Height * 2.0 / 3.0);
+
+            // Set the window size
+            this.AppWindow.Resize(new Windows.Graphics.SizeInt32(initialWidth, initialHeight));
+
+            // Center the window on screen
+            int x = (workArea.Width - initialWidth) / 2;
+            int y = (workArea.Height - initialHeight) / 2;
+            this.AppWindow.Move(new Windows.Graphics.PointInt32(x, y));
+        }
+
+        private void HomePageView_SizeChanged(object sender, WindowSizeChangedEventArgs args)
+        {
+            // Reset the timer each time the window is resized
+            // This ensures we only check minimum size after resizing stops
+            _resizeTimer.Stop();
+            _resizeTimer.Start();
+        }
+
+        private void ResizeTimer_Tick(object sender, object e)
+        {
+            // Stop the timer
+            _resizeTimer.Stop();
+
+            // Enforce minimum window size (1/3 of screen)
+            var displayArea = DisplayArea.Primary;
+            var workArea = displayArea.WorkArea;
+
+            int minWidth = (int)(workArea.Width / 3.0);
+            int minHeight = (int)(workArea.Height / 3.0);
+
+            var currentSize = this.AppWindow.Size;
+            bool needsResize = false;
+            int newWidth = currentSize.Width;
+            int newHeight = currentSize.Height;
+
+            if (currentSize.Width < minWidth)
+            {
+                newWidth = minWidth;
+                needsResize = true;
+            }
+
+            if (currentSize.Height < minHeight)
+            {
+                newHeight = minHeight;
+                needsResize = true;
+            }
+
+            if (needsResize)
+            {
+                this.AppWindow.Resize(new Windows.Graphics.SizeInt32(newWidth, newHeight));
+            }
         }
 
         /// <summary>
